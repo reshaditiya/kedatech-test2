990e6daa910c8ab829af62029e22e8a4bb17578f
reshaditiya <reshaditiya@gmail.com>
2023-08-29 08:07:03 +0700

    variant outline

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,7 +17,8 @@ export function Button({
 		primary:
 			'flex gap-2 items-center py-4 px-12 bg-red-900 text-white rounded-2xl font-display font-semibold hover:bg-red-950 active:bg-red-950 cursor-pointer transition',
 		link: 'font-display font-semibold text-red-900 hover:border-b-2 hover:border-red-900 transition',
-		outline: '',
+		outline:
+			'flex gap-2 items-center py-4 px-12 border border-red-900 text-red-900 rounded-2xl font-display font-semibold hover:bg-red-50 active:bg-red-100 cursor-pointer transition',
 		secondary: '',
 	};
 
