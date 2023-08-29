83b0b3efec11aad31fd42d2b9d36085a3eb3a40a
reshaditiya <reshaditiya@gmail.com>
2023-08-29 09:00:59 +0700

    prettier

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,31 +1,31 @@
 import { twMerge } from 'tailwind-merge';
 
 export function Button({
-	icon,
-	variant = 'primary',
-	children,
-	className,
-	...props
+  icon,
+  variant = 'primary',
+  children,
+  className,
+  ...props
 }: {
-	icon?: React.ReactElement;
-	variant?: 'primary' | 'link' | 'outline' | 'secondary';
-	children: string;
-	className?: string;
-	props?: React.ReactPropTypes;
+  icon?: React.ReactElement;
+  variant?: 'primary' | 'link' | 'outline' | 'secondary';
+  children: string;
+  className?: string;
+  props?: React.ReactPropTypes;
 }) {
-	const classVariant = {
-		primary:
-			'flex gap-2 items-center py-4 px-12 bg-red-900 text-white rounded-2xl font-display font-semibold hover:bg-red-950 active:bg-red-950 cursor-pointer transition',
-		link: 'font-display font-semibold text-red-900 hover:border-b-2 hover:border-red-900 transition',
-		outline:
-			'flex gap-2 items-center py-4 px-12 border border-red-900 text-red-900 rounded-2xl font-display font-semibold hover:bg-red-50 active:bg-red-100 cursor-pointer transition',
-		secondary: '',
-	};
+  const classVariant = {
+    primary:
+      'flex gap-2 items-center py-4 px-12 bg-red-900 text-white rounded-2xl font-display font-semibold hover:bg-red-950 active:bg-red-950 cursor-pointer transition',
+    link: 'font-display font-semibold text-red-900 hover:border-b-2 hover:border-red-900 transition',
+    outline:
+      'flex gap-2 items-center py-4 px-12 border border-red-900 text-red-900 rounded-2xl font-display font-semibold hover:bg-red-50 active:bg-red-100 cursor-pointer transition',
+    secondary: '',
+  };
 
-	return (
-		<button className={twMerge(classVariant[variant], className)} {...props}>
-			{children}
-			{icon}
-		</button>
-	);
+  return (
+    <button className={twMerge(classVariant[variant], className)} {...props}>
+      {children}
+      {icon}
+    </button>
+  );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,28 +2,28 @@ import { Button } from './button';
 import { Receipt, ArrowRight } from '@phosphor-icons/react';
 
 export function Navbar() {
-	return (
-		<nav className='flex justify-between p-6'>
-			<span className='flex items-center gap-2 font-bold font-display text-lg text-slate-800'>
-				<Receipt className='w-6 h-6' weight='bold' />
-				UpTech
-			</span>
-			<span className='hidden md:flex md:gap-12 items-center'>
-				<span className='hidden lg:flex lg: gap-12 h-full items-center'>
-					<a href='#'>
-						<Button variant='link'>Tentang</Button>
-					</a>
-					<a href='#'>
-						<Button variant='link'>Harga</Button>
-					</a>
-					<a href='#'>
-						<Button variant='link'>Kontak</Button>
-					</a>
-				</span>
-				<Button icon={<ArrowRight className='w-5 h-5' weight='bold' />}>
-					Masuk
-				</Button>
-			</span>
-		</nav>
-	);
+  return (
+    <nav className="flex justify-between px-14 py-6">
+      <span className="flex items-center gap-2 font-display text-lg font-bold text-slate-800">
+        <Receipt className="h-6 w-6" weight="bold" />
+        UpTech
+      </span>
+      <span className="hidden items-center md:flex md:gap-12">
+        <span className="hidden h-full items-center gap-12 lg:flex">
+          <a href="#">
+            <Button variant="link">Tentang</Button>
+          </a>
+          <a href="#">
+            <Button variant="link">Harga</Button>
+          </a>
+          <a href="#">
+            <Button variant="link">Kontak</Button>
+          </a>
+        </span>
+        <Button icon={<ArrowRight className="h-5 w-5" weight="bold" />}>
+          Masuk
+        </Button>
+      </span>
+    </nav>
+  );
 }
