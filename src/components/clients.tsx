c4fcd53266c59b9e49766b90769226faa140f0e1
reshaditiya <reshaditiya@gmail.com>
2023-08-29 15:16:35 +0700

    change border color

--- src/components/clients.tsx
+++ src/components/clients.tsx
@@ -1,6 +1,6 @@
 export function Clients() {
   return (
-    <section className="mx-7 my-7 flex flex-wrap items-center justify-center gap-12 border-y border-slate-200 py-7 md:mx-8 md:my-10 md:py-10 lg:mx-14 lg:my-14 lg:gap-28 lg:py-14">
+    <section className="mx-7 my-7 flex flex-wrap items-center justify-center gap-12 border-y-2 border-slate-50 py-7 md:mx-8 md:my-10 md:py-10 lg:mx-14 lg:my-14 lg:gap-28 lg:py-14">
       <img src="/clients-logo/airbnb.svg" alt="airbnb" />
       <img src="/clients-logo/google.svg" alt="google" />
       <img src="/clients-logo/amazon.svg" alt="amazon" />
