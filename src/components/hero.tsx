52141e841b06ff9f808234bfb301e1e8b3033590
reshaditiya <reshaditiya@gmail.com>
2023-08-29 14:56:58 +0700

    media cloud

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,9 +5,9 @@ export function Hero() {
   return (
     <section
       className="
-			flex flex-col items-center gap-8 overflow-hidden px-7 py-14 md:px-8 md:py-24 lg:flex-row lg:px-14 lg:py-28"
+			relative flex flex-col items-center gap-8 overflow-hidden px-7 py-14 md:px-8 md:py-24 lg:flex-row lg:px-14 lg:py-28"
     >
-      <div>
+      <div className="max-w-2xl">
         <h1 className="text-center font-display text-4xl font-bold leading-tight text-slate-950 md:text-5xl md:leading-tight lg:text-left lg:text-7xl lg:leading-tight">
           Optimalkan Bisnis Anda dengan UpTech
         </h1>
@@ -22,7 +22,11 @@ export function Hero() {
           <Button variant="outline">Hubungi Kami</Button>
         </div>
       </div>
-      <img src="/hero-image.webp" alt="" />
+      <img
+        src="clouds/cloud-hero.svg"
+        alt=""
+        className="absolute bottom-0 right-0 -z-10"
+      />
     </section>
   );
 }
