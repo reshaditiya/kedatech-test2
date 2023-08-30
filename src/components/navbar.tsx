04d30f7ede318a6e8bf8f2a4b44c775734f4fed2
reshaditiya <reshaditiya@gmail.com>
2023-08-30 09:20:06 +0700

    responsive modal

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,8 @@
+import { Menu, Transition } from '@headlessui/react';
 import { Button } from './button';
 import { Receipt, ArrowRight } from '@phosphor-icons/react';
+import { Fragment } from 'react';
+import { List } from '@phosphor-icons/react';
 
 function smoothScroll(
   e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -25,22 +28,118 @@ export function Navbar() {
         <Receipt className="h-6 w-6" weight="bold" />
         UpTech
       </span>
-      <span className="hidden items-center md:flex md:gap-12">
-        <span className="hidden h-full items-center gap-12 lg:flex">
-          <a href="/" onClick={(e) => smoothScroll(e, 'about')}>
-            <Button variant="link">Tentang</Button>
-          </a>
-          <a href="/" onClick={(e) => smoothScroll(e, 'pricing')}>
-            <Button variant="link">Harga</Button>
-          </a>
-          <a href="/" onClick={(e) => smoothScroll(e, 'contact')}>
-            <Button variant="link">Kontak</Button>
-          </a>
-        </span>
-        <Button icon={<ArrowRight className="h-5 w-5" weight="bold" />}>
+      <span className="hidden items-center md:gap-12 lg:flex">
+        <a href="/" onClick={(e) => smoothScroll(e, 'about')}>
+          <Button variant="link">Tentang</Button>
+        </a>
+        <a href="/" onClick={(e) => smoothScroll(e, 'pricing')}>
+          <Button variant="link">Harga</Button>
+        </a>
+        <a href="/" onClick={(e) => smoothScroll(e, 'contact')}>
+          <Button variant="link">Kontak</Button>
+        </a>
+        <Button>
           Masuk
+          <ArrowRight className="h-5 w-5" weight="bold" />
         </Button>
       </span>
+      <MobileNav />
     </nav>
+  );
+}
+
+function MobileNav() {
+  return (
+    <div className="lg:hidden">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button>
+            <Button variant="icon">
+              <List className="h-6 w-6" weight="bold" />
+            </Button>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-3xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="flex flex-col gap-2 px-4 py-4">
+              <Menu.Item>
+                {({ close }) => (
+                  <a
+                    href="/"
+                    onClick={(e) => {
+                      smoothScroll(e, 'about');
+                      close();
+                    }}
+                  >
+                    <Button
+                      variant="ghost"
+                      className="flex w-full justify-center py-3"
+                    >
+                      Tentang
+                    </Button>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ close }) => (
+                  <a
+                    href="/"
+                    onClick={(e) => {
+                      close();
+                      smoothScroll(e, 'pricing');
+                    }}
+                  >
+                    <Button
+                      variant="ghost"
+                      className="flex w-full justify-center py-3"
+                    >
+                      Harga
+                    </Button>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ close }) => (
+                  <a
+                    href="/"
+                    onClick={(e) => {
+                      close();
+                      smoothScroll(e, 'contact');
+                    }}
+                  >
+                    <Button
+                      variant="ghost"
+                      className="flex w-full justify-center py-3"
+                    >
+                      Kontak
+                    </Button>
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ close }) => (
+                  <Button
+                    variant="primary"
+                    className="flex w-full justify-center py-3"
+                    onClick={close}
+                  >
+                    Masuk
+                    <ArrowRight className="h-5 w-5" weight="bold" />
+                  </Button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
   );
 }
